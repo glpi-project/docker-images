5753c58b47f0adb39e949cf61130e3d5b34c4c02
Alexandre Delaunay <adelaunay@teclib.com>
2025-07-02 14:04:30 +0200

    Forward GLPI logs to docker logs

--- glpi/files/opt/startup.sh
+++ glpi/files/opt/startup.sh
@@ -3,23 +3,23 @@
 # Create `config`, `marketplace` and `files` volume (sub)directories that are missing
 # and set ACL for www-data user
 dirs=(
-    "/var/glpi/config"
-    "/var/glpi/files"
-    "/var/glpi/files/_cache"
-    "/var/glpi/files/_cron"
-    "/var/glpi/files/_dumps"
-    "/var/glpi/files/_graphs"
-    "/var/glpi/files/_locales"
-    "/var/glpi/files/_lock"
-    "/var/glpi/files/_log"
-    "/var/glpi/files/_pictures"
-    "/var/glpi/files/_plugins"
-    "/var/glpi/files/_rss"
-    "/var/glpi/files/_sessions"
-    "/var/glpi/files/_tmp"
-    "/var/glpi/files/_uploads"
-    "/var/glpi/files/_inventories"
-    "/var/glpi/marketplace"
+    "${GLPI_CONFIG_DIR}"
+    "${GLPI_VAR_DIR}"
+    "${GLPI_VAR_DIR}/_cache"
+    "${GLPI_VAR_DIR}/_cron"
+    "${GLPI_VAR_DIR}/_dumps"
+    "${GLPI_VAR_DIR}/_graphs"
+    "${GLPI_VAR_DIR}/_locales"
+    "${GLPI_VAR_DIR}/_lock"
+    "${GLPI_VAR_DIR}/_pictures"
+    "${GLPI_VAR_DIR}/_plugins"
+    "${GLPI_VAR_DIR}/_rss"
+    "${GLPI_VAR_DIR}/_sessions"
+    "${GLPI_VAR_DIR}/_tmp"
+    "${GLPI_VAR_DIR}/_uploads"
+    "${GLPI_VAR_DIR}/_inventories"
+    "${GLPI_MARKETPLACE_DIR}"
+    "${GLPI_LOG_DIR}"
 )
 for dir in "${dirs[@]}"
 do
@@ -33,6 +33,46 @@ do
     chmod u+rwx "$dir"
     find "$dir" -type d -exec chmod u+rwx {} \;
     find "$dir" -type f -exec chmod u+rw {} \;
+done
+
+# forward logs files to stdout/stderr
+# see https://stackoverflow.com/a/63713129
+info_logs=(
+    "${GLPI_LOG_DIR}/event.log"
+    "${GLPI_LOG_DIR}/cron.log"
+    "${GLPI_LOG_DIR}/mail.log"
+)
+error_logs=(
+    "${GLPI_LOG_DIR}/php-errors.log"
+    "${GLPI_LOG_DIR}/sql-errors.log"
+    "${GLPI_LOG_DIR}/mail-errors.log"
+    "${GLPI_LOG_DIR}/access-errors.log"
+)
+# Create log files if they do not exist and set rights for www-data user
+all_logs=(
+    "${info_logs[@]}"
+    "${error_logs[@]}"
+)
+for log in "${all_logs[@]}"
+do
+    if [ ! -f $log ];
+    then
+        touch "$log"
+        chown www-data:www-data "$log"
+        chmod u+rw "$log"
+    fi
+done
+
+# info log files to stdout
+for log in "${info_logs[@]}"
+do
+    tail -F "$log" > /proc/1/fd/1 &
+done
+
+# error log files to stderr
+for log in "${error_logs[@]}"
+do
+    tail -F "$log" > /proc/1/fd/2 &
 done
 
 # Run cron service.
