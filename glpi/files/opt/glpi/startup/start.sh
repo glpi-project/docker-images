1929c201fbf78d272a67b5962abfcf5dbe61a28c
Cédric Anne <cedric.anne@gmail.com>
2025-12-15 12:20:37 +0100

    Fix `GLPI_*` env vars in cron context (#238)
    * Handle env variables at runtime
    * Put only GLPI env vars in `/etc/environment`

--- glpi/files/opt/glpi/startup/start.sh
+++ glpi/files/opt/glpi/startup/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e -u -o pipefail
 
+# Copy the GLPI env variables to `/etc/environment`, to make them available for the commands executed by the cron service
+# using the `www-data` user.
+printenv | grep 'GLPI_' > /etc/environment
+
 # Run cron service.
 cron
 
