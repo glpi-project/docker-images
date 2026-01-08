3a4d5135e4257e48cc32be633763df310b552e35
Benoit VIGNAL <git@benoit-vignal.fr>
2026-01-08 14:02:40 +0100

    Add script to wait for DB to be ready before doing GLPI actions

--- glpi/files/opt/glpi/cron-worker.sh
+++ glpi/files/opt/glpi/cron-worker.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e -u -o pipefail
 
+# Wait for database to be ready before starting cron tasks
+/opt/glpi/entrypoint/wait-for-db.sh cron-worker
+
 # Infinite loop to run GLPI cron tasks every minute
 while true; do
     php /var/www/glpi/front/cron.php || echo "Cron task failed"
