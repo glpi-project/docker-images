0773cde62a8002fb87d020f0950120d2aad41722
Benoit VIGNAL <git@benoit-vignal.fr>
2026-01-14 15:06:04 +0100

    Marketplace path is check on image entrypoint

--- glpi/files/opt/glpi/entrypoint.sh
+++ glpi/files/opt/glpi/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e -u -o pipefail
 
-# Source GLPI environment variables (includes GLPI_MARKETPLACE_DIR detected at build time)
-if [ -f /etc/glpi_env ]; then
-  export $(cat /etc/glpi_env | xargs)
+if [ -z "${GLPI_MARKETPLACE_DIR:-}" ]; then
+    GLPI_MAJOR=$(ls /var/www/glpi/version/ | sort --version-sort | tail --lines=1| cut --delimiter='.' --fields=1)
+    if [ "$GLPI_MAJOR" = "10" ]; then
+      export GLPI_MARKETPLACE_DIR="/var/www/glpi/marketplace"
+    else
+      export GLPI_MARKETPLACE_DIR="/var/glpi/marketplace"
+    fi
 fi
 
 /opt/glpi/entrypoint/init-volumes-directories.sh
