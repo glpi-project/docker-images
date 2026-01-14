07ca706ce5b63c5bcf6a0d68de93f99324a05044
Benoit VIGNAL <git@benoit-vignal.fr>
2026-01-14 12:29:10 +0100

    Move MARKETPLACE_DIR into Dockerfile so 10.x is supported

--- glpi/files/opt/glpi/entrypoint.sh
+++ glpi/files/opt/glpi/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e -u -o pipefail
 
+# Source GLPI environment variables (includes GLPI_MARKETPLACE_DIR detected at build time)
+if [ -f /etc/glpi_env ]; then
+  export $(cat /etc/glpi_env | xargs)
+fi
+
 /opt/glpi/entrypoint/init-volumes-directories.sh
 /opt/glpi/entrypoint/forward-logs.sh
 /opt/glpi/entrypoint/wait-for-db.sh entrypoint
