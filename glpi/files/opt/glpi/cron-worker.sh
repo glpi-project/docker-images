0fcc309a9035413edcbcfdd8ab251900d1cd7ac5
Alejandro Navarro <a.navarro.montiel@gmail.com>
2026-01-15 11:50:08 +0100

    Update cron-worker.sh

--- glpi/files/opt/glpi/cron-worker.sh
+++ glpi/files/opt/glpi/cron-worker.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 set -e -u -o pipefail
 
-if [ "${ENABLE_CRONTAB:-1}" = "1" ]; then
-    echo "[INFO] Cron ENABLED. Starting worker tasks..."
+if [ "${GLPI_CRONTAB_ENABLED:-1}" = "1" ]; then
+    echo "[INFO] GLPI cron is enabled. Starting worker tasks..."
     
     # Wait for database to be ready before starting cron tasks
     /opt/glpi/entrypoint/wait-for-db.sh cron-worker
     
     # Infinite loop to run GLPI cron tasks every minute
     while true; do
-        php /var/www/glpi/front/cron.php || echo "[ERROR] GLPI cron task failed"
+        php /var/www/glpi/front/cron.php || echo "[ERROR] GLPI cron execution failed"
         sleep 60
     done
 
 else
     # Infinite loop to run GLPI cron tasks every minute
-    echo "[INFO] Cron DISABLED (ENABLE_CRONTAB=0)."
-    echo "[INFO] Sleeping forever to keep supervisord happy..."
-    
+    echo "[INFO] GLPI cron is disabled (GLPI_CRONTAB_ENABLED=0)."
+    # Sleeping forever to keep supervisord happy    
     tail -f /dev/null
 fi
